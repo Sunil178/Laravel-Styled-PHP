51f584f662bb81cec20ab377cbe7fb9fdc275640
Sunil Thakur <sunilthakur178co@gmail.com>
2023-01-25 06:02:30 +0530

    Fixed logo link

--- layout/sidebar.php
+++ layout/sidebar.php
@@ -1,6 +1,6 @@
 <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
     <div class="app-brand demo">
-        <a href="index.php" class="app-brand-link">
+        <a href=".." class="app-brand-link">
             <span class="app-brand-logo demo">
                 <img src="./../assets/img/aceaffilino-logo.jpg" alt=""
                     style="width: 6%; margin: 6px; margin-left: 37px;">
